ff9bae26d9f4f139e72bfcc7f4118d7e107c3395
Igor Ramos De Luna <igor.lunarx@gmail.com>
2016-09-01 15:41:10 -0300

    Adicionando branch de desenvolvimento

--- AgendaCorporativa/AgendaCorporativa/AgendaCorporativa/App.cs
+++ AgendaCorporativa/AgendaCorporativa/AgendaCorporativa/App.cs
@@ -20,7 +20,7 @@ namespace AgendaCorporativa
                     Children = {
                         new Label {
                             HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
+                            Text = "Welcome to Xamarin Forms!" 
                         }
                     }
                 }
