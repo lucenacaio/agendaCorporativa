e1ee06722046b59c41bc78b6fd5d2177f95dd259
Igor Ramos De Luna <igor.lunarx@gmail.com>
2016-09-05 10:41:05 -0300

    Adicionando metodo de obter o conteudo do arquivo
    Related Work Items: #96041, #96044

---
+++ AgendaCorporativa/AgendaCorporativa/AgendaCorporativa/Gerenciadores/GerenciadorDeArquivo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaCorporativa.Gerenciadores
+{
+    class GerenciadorDeArquivo
+    {
+        //URL do Arquivo
+        public Uri uri = new Uri("http://localhost:9090/img/teste.txt");
+
+        public async Task<string> BaixarConteudoDoArquivo()
+        {
+            var httpClient = new HttpClient();
+
+            //Baixa o arquivo
+            byte[] arquivo = await httpClient.GetByteArrayAsync(uri);
+            
+            //Converte de Bytes para String
+            string conteudoDoArquivo = Encoding.UTF8.GetString(arquivo, 0, arquivo.Length);
+
+            return conteudoDoArquivo;
+        }
+    }
+}
