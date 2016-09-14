38456f541e2754cacf39e7f37190b3d3fe90b710
Igor Ramos De Luna <igor.lunarx@gmail.com>
2016-09-14 16:05:36 -0300

    Corrigindo problema de performance ao pesquisar; Refatorando código da tela principal;
    Related Work Items: #96030

---
+++ AgendaCorporativa/AgendaCorporativa/AgendaCorporativa/Extentions/ListExtentions.cs
@@ -0,0 +1,45 @@
+﻿using AgendaCorporativa.Modelos;
+using Plugin.Contacts.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaCorporativa.Extentions
+{
+    public static class ListExtentions
+    {
+        public static IEnumerable<Contato> ToListaDeContato(this IEnumerable<Contact> contatos)
+        {
+            List<Contato> contatosLista = new List<Contato>();
+
+            foreach (Contact contato in contatos)
+            {
+                List<Telefone> telefones = new List<Telefone>();
+                List<string> emails = new List<string>();
+
+                foreach (Phone phone in contato.Phones)
+                {
+                    Telefone telefone = new Telefone();
+                    telefone.Numero = phone.Number;
+                    telefones.Add(telefone);
+                }
+
+                foreach (Email email in contato.Emails)
+                {
+                    emails.Add(email.Address);
+                }
+
+                Contato cont = new Contato();
+                cont.NomeFuncionario = contato.DisplayName;
+                cont.Telefones = telefones;
+                cont.Emails = emails;
+
+                contatosLista.Add(cont);
+            }
+
+            return contatosLista;
+        }
+    }
+}
---
+++ AgendaCorporativa/Stefanini.Xamarin/Gerenciadores/GerenciadorDeAgenda.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.Contacts;
+using Plugin.Contacts.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stefanini.Xamarin.Gerenciadores
+{
+    public class GerenciadorDeAgenda
+    {
+        /// <summary>
+        /// Obtem uma lista de <see cref="Contact"/> da agenda do celular
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<List<Contact>> CarregaAgendaDoAparelho()
+        {
+            List<Contact> contatos = null;
+            if (await CrossContacts.Current.RequestPermission())
+            {
+                CrossContacts.Current.PreferContactAggregation = false;
+
+                await Task.Run(() =>
+                {
+                    if (CrossContacts.Current.Contacts == null)
+                        return;
+
+                    var contacts = CrossContacts.Current.Contacts
+                                            .Where(c => !string.IsNullOrWhiteSpace(c.FirstName) && c.Phones.Count > 0);
+
+                    contatos = contacts.OrderBy(c => c.FirstName).ToList();
+                });
+            }
+
+            return contatos;
+        }
+    }
+}
